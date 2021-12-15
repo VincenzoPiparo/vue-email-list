6f37adfbce34fc080f572869cab17a6a302159b3
VincenzoPiparo <v.pipar.97@gmai.com>
2021-12-15 15:53:35 +0100

    SCAFFOLDING

---
+++ js/script.js
@@ -0,0 +1,26 @@
+Vue.config.devtools = true;
+// Descrizione:
+
+// Attraverso l'apposita API di Boolean
+
+// https://flynn.boolean.careers/exercises/api/random/mail
+
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus:
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue (
+    {
+        el: '#root',
+        data:{
+
+        },
+        methods: {
+
+        },
+        created: function() {
+
+        }
+}
+)
